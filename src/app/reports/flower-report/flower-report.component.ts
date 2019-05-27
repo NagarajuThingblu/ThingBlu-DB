24b1cb0d745fcb2e803edbcbb8ed805b5bd4cddb
Sanjay <sanjay.kurane@thingblu.com>
2019-05-27 18:37:30 +0530

    Changed embeded reports link after unit testing

---
+++ src/app/reports/flower-report/flower-report.component.ts
@@ -0,0 +1,61 @@
+import { DomSanitizer } from '@angular/platform-browser';
+import { AppCommonService } from '../../shared/services/app-common.service';
+import { LoaderService } from '../../shared/services/loader.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flower-report',
+  templateUrl: './flower-report.component.html',
+  styles: [`
+  iframe #WoskSpaceDiv {
+    width:100% !important;
+  }
+
+  .clsframe{
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+  }
+  `]
+})
+export class FlowerReportComponent implements OnInit {
+  public iFrameUrl: any;
+  public value = 14;
+  public userName: any;
+  reportUrl = {
+    rptUrl: null
+  };
+  video: any;
+  constructor(
+    private loaderService: LoaderService,
+    private appCommonService: AppCommonService,
+    public sanitizer: DomSanitizer
+
+  ) { }
+
+  ngOnInit() {
+    this.loaderService.display(true);
+    //  setTimeout(() => {
+    //   this.loaderService.display(false);
+    //  }, 5000);
+    this.userName = this.appCommonService.getUserProfile().UserName;
+    if (this.userName) {
+      this.iFrameUrl = 'https://demo.navizanalytics.com/NavizUICW/DashboardEmbed/Home.aspx?DashboardName=Flower&UserName=' + this.userName;
+ // this.iFrameUrl = 'https://demo.navizanalytics.com/NavizUICW/DashboardEmbed/Home.aspx?DashboardName=Flower&UserName=' + this.value;
+      this.iFrameUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.iFrameUrl);
+      this.reportUrl.rptUrl = this.iFrameUrl;
+      this.loaderService.display(false);
+    }
+  }
+
+  getUrl(event) {
+    //  if (this.Istrue) {
+    //    this.Istrue = false;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.iFrameUrl);
+    //  }
+
+  }
+
+}
