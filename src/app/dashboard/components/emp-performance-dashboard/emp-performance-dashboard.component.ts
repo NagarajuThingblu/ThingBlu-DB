43be25f4c756d3290a18cbee8283a810a8227a1c
Nagamanikanta <nagamanikanta@thingblu.com>
2020-12-26 19:52:56 +0530

    employee performance dashboard changes

--- src/app/dashboard/components/emp-performance-dashboard/emp-performance-dashboard.component.ts
+++ src/app/dashboard/components/emp-performance-dashboard/emp-performance-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LoaderService } from '../../../shared/services/loader.service';
 import { DashobardService } from '../../services/dashobard.service';
+import { SelectItem } from 'primeng/api';
+import { DropdwonTransformService } from '../../../shared/services/dropdown-transform.service';
+import { DropdownValuesService } from '../../../shared/services/dropdown-values.service';
 
 @Component({
   selector: 'app-emp-performance-dashboard',
@@ -8,23 +11,52 @@ import { DashobardService } from '../../services/dashobard.service';
   styleUrls: ['./emp-performance-dashboard.component.css']
 })
 export class EmpPerformanceDashboardComponent implements OnInit {
+  dropdowndata:any;
   data: any;
+  public taskTypes: SelectItem[];
   Tasktypetotaltime:any;
-  constructor(private loaderService: LoaderService,private dashboardService: DashobardService) { 
+  constructor(private loaderService: LoaderService,private dashboardService: DashobardService,  private dropdownDataService: DropdownValuesService,
+    private dropdwonTransformService: DropdwonTransformService,) { 
     this.data = {
-      labels: ['A','B','C'],
+      labels: ['A','B','C','D','E','F','G','H','abc bcd efg','ijk klm mno','jyothi','abc icd ehg','ijk hjhjlm mno','jyothi1','mani'],
       datasets: [
           {
-              data: [300, 50, 100],
+              data: [300, 50, 100,300, 50, 100,300, 50, 100,300, 50, 100,300, 50, 100],
               backgroundColor: [
                   "#FF6384",
                   "#36A2EB",
-                  "#FFCE56"
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                 
+                 
               ],
               hoverBackgroundColor: [
                   "#FF6384",
                   "#36A2EB",
-                  "#FFCE56"
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
               ]
           }]    
       };
@@ -32,8 +64,11 @@ export class EmpPerformanceDashboardComponent implements OnInit {
 
   ngOnInit() {
     this.loaderService.display(false);
+    this.getAllTaskTypes();
     this.GetTaskTypetotalTime();
-    
+    this.dropdowndata = [{ id:1, Title:"Last Week"},
+                           { id:2, Title:"YTD"},
+                           { id:3, Title:"MTD"}]
   }
 
   GetTaskTypetotalTime()
@@ -41,14 +76,37 @@ export class EmpPerformanceDashboardComponent implements OnInit {
 this.dashboardService.GetTasktypeTotaltime().subscribe(data=>{
   this.Tasktypetotaltime=data;
   this.data = {
-    labels: this.Tasktypetotaltime.TaskTypeName,
+    labels: this.Tasktypetotaltime[0],
     datasets: [
         {
-            data: this.Tasktypetotaltime.TotalTime
+            data: this.Tasktypetotaltime[1],
+            backgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+              "#FF6384",
+              "#36A2EB",
+              "#FFCE56"
+          ]
             
         }]    
     };
 })
   }
+
+  getAllTaskTypes() {
+    // this.loaderService.display(true);
+     this.dropdownDataService.getAllTask().subscribe(
+       data => {
+         // console.log(data);
+        //  this.globalData.taskTypes = data;
+         this.taskTypes = this.dropdwonTransformService.transform(data, 'TaskTypeName', 'TaskTypeId', 'Show All', false) ;
+        // this.loaderService.display(false);
+       } ,
+       error => { console.log(error); this.loaderService.display(false); },
+       );
+   }
 
 }
